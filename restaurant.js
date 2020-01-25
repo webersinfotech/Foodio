d45b5626f72c3eb0e3272ef2293768c84ddc1307
Rushabh Shah <webersinfotech@gmail.com>
2020-01-25 09:59:09 +0530

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -40,7 +40,7 @@ class Resturant {
             try {
                 const data = await this.fetchDetail(resturant.sName, resturant.area.entity_id, resturant.area.entity_type);
                 // console.log(data.status);
-                // console.log(data);
+                console.log(data);
                 try {
                     const urls = [];
                     
@@ -73,7 +73,6 @@ class Resturant {
             // 4cbd22ad6953d19f5ee877615a7c9cc5
 
             request(options, function (error, response, body) {
-                console.log(response);
                 if (error) rej(error);
                 res(JSON.parse(body));
             });
