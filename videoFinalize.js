24ee4e5cf58e667982c6450cc53efa7f803ddd60
Rushabh Shah <webersinfotech@gmail.com>
2020-03-07 23:57:42 +0530

    Update videoFinalize.js

--- videoFinalize.js
+++ videoFinalize.js
@@ -4,29 +4,52 @@ const ffmpeg = require('fluent-ffmpeg');
 
 
 (async () => {
-    new ffmpeg()
-    .addInput(`${__dirname}/assets/1582904095539.webm`)
-    .addInput(`${__dirname}/assets/bensound-perception.mp3`)
-    .outputOptions(['-shortest'])
-    .saveToFile(`${__dirname}/assets/output.mp4`).on('progress', function(progress) {
-        console.log(progress);
-        // console.log('Processing: ' + progress.percent + '% done');
+    try {
+        await mongoose.connect('mongodb://foodioadmin:11999966@15.206.164.241:27017/Foodio', { promiseLibrary: global.Promise, useNewUrlParser: true });
+    } catch(error) {
+        console.log(error);
+    }
+
+    const query = [{
+        $match: {
+            bIsIntroCaptured: true
+        }
+    }, {
+        $limit: 1
+    }];
+
+    const data = await mongo.fetchRestaurantsAggregate(query);
+
+    asyncForEach(data, (res) => {
+        try {
+            const name = res.videoUrl.split('/')[1];
+            await prepareVideo(name);
+            console.log(`${res.videoUrl.split('/')[0]}/ready/${name.split('.')[0]}-ready.mp4`);
+            await mongo.updateRestaurant({_id: id}, {
+                bIsVideoReady: true,
+                readyVideoUrl: `${res.videoUrl.split('/')[0]}/ready/${name.split('.')[0]}-ready.mp4`
+            });
+        } catch(error) {
+            console.log(error);
+        }
     });
-    // try {
-    //     await mongoose.connect('mongodb://foodioadmin:11999966@15.206.164.241:27017/Foodio', { promiseLibrary: global.Promise, useNewUrlParser: true });
-    // } catch(error) {
-    //     console.log(error);
-    // }
-
-    // const query = [{
-    //     $match: {
-    //         bIsIntroCaptured: true
-    //     }
-    // }, {
-    //     $limit: 1
-    // }];
-
-    // const data = await mongo.fetchRestaurantsAggregate(query);
-
-    // console.log(data);
 })();
+
+async function prepareVideo(name) {
+    return new promise((res, rej) => {
+        new ffmpeg()
+        .addInput(`${__dirname}/assets/${name}`)
+        .addInput(`${__dirname}/assets/bensound-perception.mp3`)
+        .outputOptions(['-shortest'])
+        .saveToFile(`${__dirname}/assets/ready/${name.split('.')[0]}-ready.mp4`)
+        .on('progress', (progress) => console.log(progress))
+        .on('end', () => res())
+        .on('error', (error) => rej(error));
+    })
+}
+
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
