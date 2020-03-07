6962cefa83d970e1f078bb9992e74cd13b9849fc
Rushabh Shah <webersinfotech@gmail.com>
2020-03-07 22:57:15 +0530

    Update videoFinalize.js

--- videoFinalize.js
+++ videoFinalize.js
@@ -4,12 +4,12 @@ const ffmpeg = require('fluent-ffmpeg');
 
 
 (async () => {
-    new ffmpeg().on('progress', function(progress) {
-        console.log('Processing: ' + progress.percent + '% done');
-    })
+    new ffmpeg()
     .addInput(`${__dirname}/assets/1582904095539.webm`)
     .addInput(`${__dirname}/assets/bensound-perception.mp3`)
-    .saveToFile(`${__dirname}/assets/output.webm`);
+    .saveToFile(`${__dirname}/assets/output.mp4`).on('progress', function(progress) {
+        console.log('Processing: ' + progress.percent + '% done');
+    });
     // try {
     //     await mongoose.connect('mongodb://foodioadmin:11999966@15.206.164.241:27017/Foodio', { promiseLibrary: global.Promise, useNewUrlParser: true });
     // } catch(error) {
