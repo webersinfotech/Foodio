618b4901bf38a06d29f2aa25e6758968bb5ad2f5
Rushabh Shah <webersinfotech@gmail.com>
2020-03-07 22:25:57 +0530

    video finalize

--- videoFinalize.js
+++ videoFinalize.js
@@ -1,22 +1,28 @@
 const mongoose = require('mongoose');
 const mongo = require('./mongo');
+var ffmpeg = require('fluent-ffmpeg');
+
 
 (async () => {
-    try {
-        await mongoose.connect('mongodb://foodioadmin:11999966@15.206.164.241:27017/Foodio', { promiseLibrary: global.Promise, useNewUrlParser: true });
-    } catch(error) {
-        console.log(error);
-    }
+    new FFmpeg()
+    .addInput(`${__dirname}/assets/1582904095539.webm`)
+    .addInput(`${__dirname}/assets/bensound-perception.mp3`)
+    .saveToFile(`${__dirname}/assets/output.mp4`);
+    // try {
+    //     await mongoose.connect('mongodb://foodioadmin:11999966@15.206.164.241:27017/Foodio', { promiseLibrary: global.Promise, useNewUrlParser: true });
+    // } catch(error) {
+    //     console.log(error);
+    // }
 
-    const query = [{
-        $match: {
-            bIsIntroCaptured: true
-        }
-    }, {
-        $limit: 1
-    }];
+    // const query = [{
+    //     $match: {
+    //         bIsIntroCaptured: true
+    //     }
+    // }, {
+    //     $limit: 1
+    // }];
 
-    const data = await mongo.fetchRestaurantsAggregate(query);
+    // const data = await mongo.fetchRestaurantsAggregate(query);
 
     console.log(data);
 })();
