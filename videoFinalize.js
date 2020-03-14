a59b0a4d1bb005d36c52d87eb11e41250c8e8524
Rushabh Shah <webersinfotech@gmail.com>
2020-03-14 23:17:08 +0530

    Update videoFinalize.js

--- videoFinalize.js
+++ videoFinalize.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mongo = require('./mongo');
 const ffmpeg = require('fluent-ffmpeg');
+const publicIp = require('public-ip');
 
 
 (async () => {
@@ -15,27 +16,30 @@ const ffmpeg = require('fluent-ffmpeg');
             bIsIntroCaptured: true,
             bIsVideoReady: {
                 $ne: true
-            }
+            },
+            videoUrl: new RegExp(`^${await publicIp.v4()}:3001`)
         }
+    }, {
+        $limit: 1
     }];
 
     const data = await mongo.fetchRestaurantsAggregate(query);
 
     console.log(data);
 
-    asyncForEach(data, async (res) => {
-        try {
-            const name = res.videoUrl.split('/')[1];
-            await prepareVideo(name);
-            console.log(`${res.videoUrl.split('/')[0]}/ready/${name.split('.')[0]}-ready.mp4`);
-            await mongo.updateRestaurant({_id: res._id}, {
-                bIsVideoReady: true,
-                readyVideoUrl: `${res.videoUrl.split('/')[0]}/ready/${name.split('.')[0]}-ready.mp4`
-            });
-        } catch(error) {
-            console.log(error);
-        }
-    });
+    // asyncForEach(data, async (res) => {
+    //     try {
+    //         const name = res.videoUrl.split('/')[1];
+    //         await prepareVideo(name);
+    //         console.log(`${res.videoUrl.split('/')[0]}/ready/${name.split('.')[0]}-ready.mp4`);
+    //         await mongo.updateRestaurant({_id: res._id}, {
+    //             bIsVideoReady: true,
+    //             readyVideoUrl: `${res.videoUrl.split('/')[0]}/ready/${name.split('.')[0]}-ready.mp4`
+    //         });
+    //     } catch(error) {
+    //         console.log(error);
+    //     }
+    // });
 })();
 
 async function prepareVideo(name) {
